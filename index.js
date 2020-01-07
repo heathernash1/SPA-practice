59f805d74b60d148b6fa1454b8e9b97eb3f97ce0
heathernash1 <nash.heatherm@gmail.com>
2020-01-06 20:25:48 -0600

    render initial SPA

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { Header, Nav, Main, Footer } from "./components";
 
-document.querySelector("#root");
+document.querySelector("#root").innerHTML = `
+${Header}
+${Nav}
+${Main}
+${Footer}
+`;
